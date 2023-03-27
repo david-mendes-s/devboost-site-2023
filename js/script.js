45e8fdc177b59f9d51057cccfbb859fb33810c5f
Jardson Martins <jardsonmartins12@gmail.com>
2023-03-26 23:30:00 -0300

    Update youtube

--- js/script.js
+++ js/script.js
@@ -15,3 +15,29 @@ var mySwiper = new Swiper('.swiper-container', {
           prevEl: '.swiper-button-prev',
       },
 });
+
+// Obtém o botão e o modal
+var botao = document.querySelector(".play");
+var modal = document.querySelector(".modal");
+var modalFechar = document.querySelector(".modal-fechar");
+
+// Adiciona um ouvinte de evento ao botão
+botao.addEventListener("click", function() {
+  // Exibe o modal
+  modal.style.display = "block";
+});
+
+// Adiciona um ouvinte de evento ao botão de fechar
+modalFechar.addEventListener("click", function() {
+  // Oculta o modal
+  modal.style.display = "none";
+});
+
+// Adiciona um ouvinte de evento ao elemento window para fechar o modal ao clicar fora dele
+window.addEventListener("click", function(event) {
+  // Verifica se o alvo do evento não é o modal nem o botão de abrir o modal
+  if (event.target == modal) {
+    // Oculta o modal
+    modal.style.display = "none";
+  }
+});
